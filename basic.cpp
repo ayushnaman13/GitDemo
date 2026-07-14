446300bc827ea32c059aa9fc303c942b5625b3c0
ayushnaman13 <ayushnamanjnv@gmail.com>
2026-07-14 11:29:36 +0530

    modified after cloning

--- basic.cpp
+++ basic.cpp
@@ -2,6 +2,12 @@
 using namespace std;
 
 int main(){
-	cout<<"Hello World";
+	cout<<"Hello World"<<endl;
+	cout<<"welcome to the world"<<endl;
+
+	int a = 10;
+	int b = 20;
+	int sum = a+b;
+	cout<<sum;
 	return 0;
 }
